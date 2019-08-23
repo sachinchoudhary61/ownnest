cb84ee4bf3e9d14b4c67023db9f16eb553faebe6
sachu <seartsachu@gmail.com>
2019-08-23 23:16:43 +0530

    Initial commit

---
+++ hotel/urls.py
@@ -0,0 +1,13 @@
+
+# from django.conf.urls import url
+# from myuser import views
+# from miscellaneous import otp_sending,test,test2
+# from django.views.generic import TemplateView
+#app_name = "myuser"
+
+urlpatterns = [
+   # url(r'^$', views.userhome),
+   # url(r'^addrole/$',views.addrole),
+
+
+]
